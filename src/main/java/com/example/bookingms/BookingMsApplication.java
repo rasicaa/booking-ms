0d585e19eaac3deaf3b1e486e008fecd599fafbf
rasica <rasika ghormode@Advertricssoft.com>
2023-03-06 15:48:42 +0530

    hiiiii

---
+++ src/main/java/com/example/bookingms/BookingMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMsApplication.class, args);
+	}
+
+}
